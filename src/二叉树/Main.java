0ae7b7a5cb9a3a1ce2f6ac486e147b66840fe766
qiangzhi <>
2021-05-03 20:23:41 +0800

    优化继承结构

--- src/二叉树/Main.java
+++ src/二叉树/Main.java
@@ -1,24 +1,7 @@
 package 二叉树;
-
-import com.sun.source.tree.BinaryTree;
 import 二叉树.printer.BinaryTrees;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.util.Comparator;
-
 public class Main {
-//    public static class PersonComparator implements Comparator<Person>{
-//        public int compare(Person e1, Person e2) {
-//            return e1.getAge() - e2.getAge();
-//        }
-//    }
-//    public static class PersonComparator2 implements Comparator<Person>{
-//        public int compare(Person e1, Person e2) {
-//            return e2.getAge() - e1.getAge();
-//        }
-//    }
-
     static void test1(){
         Integer data[] = new Integer[] {
                 7, 4, 9, 2, 5, 8, 11, 3, 12, 1
@@ -30,118 +13,12 @@ public class Main {
         }
 
         BinaryTrees.println(bst);
-    }
-
-    static void test2(){
-        Integer data[] = new Integer[] {
-                7, 4, 9, 2, 5, 8, 11, 3, 12, 1
-        };
-
-        BinarySearchTree<Person> bst1 = new BinarySearchTree<>();
-        for(int i = 0; i < data.length; i ++) {
-            bst1.add(new Person(data[i]));
-        }
-        BinaryTrees.println(bst1);
-
-
-        BinarySearchTree<Person> bst2 = new BinarySearchTree<>(new Comparator<Person>() {
-            @Override
-            public int compare(Person o1, Person o2) {
-                return o2.getAge() - o1.getAge();
-            }
-        });
-        for(int i = 0; i < data.length; i ++) {
-            bst2.add(new Person(data[i]));
-        }
-        BinaryTrees.println(bst2);
-    }
-
-    static void test3(){
-//        Integer data[] = new Integer[] {
-//                7, 4, 9, 2, 5, 8, 11, 3, 12, 1
-//        };
-//
-//        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-//        for(int i = 0; i < data.length; i ++) {
-//            bst.add(data[i]);
-//        }
-
-//        BinaryTrees.println(bst);
-//
-//        bst.levelOrderTraversal();
-
-
-
-//        bst.levelOrder(new BinarySearchTree.Visitor<Integer>() {
-//            @Override
-//            public void visit(Integer element) {
-//                System.out.println("_" + element + "_");
-//            }
-//        });
-
-//        bst.inorder(new BinarySearchTree.Visitor<Integer>() {
-//            @Override
-//            public void visit(Integer element) {
-//                System.out.println("_" + element + "_");
-//            }
-//        });
-
-//        bst.postorder(new BinarySearchTree.Visitor<Integer>() {
-//            @Override
-//            public void visit(Integer element) {
-//                System.out.println("_" + element + "_");
-//            }
-//        });
-
-//        BinaryTrees.println(bst);
-//        System.out.println(bst.height1());
-
-
-//        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-//        for(int i = 0; i < 10; i ++) {
-//            bst.add((int)(Math.random() * 100));
-//        }
-//        BinaryTrees.println(bst);
-//        System.out.println(bst.isComplete());
-
-
-        Integer data[] = new Integer[] {
-                7, 4, 9, 2, 5
-        };
-
-        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-        for(int i = 0; i < data.length; i ++) {
-            bst.add(data[i]);
-        }
-        BinaryTrees.println(bst);
-        System.out.println(bst.isComplete());
-    }
-
-    static void test7(){
-        Integer data[] = new Integer[] {
-                7, 4, 9, 2, 5, 8, 11, 3, 12, 1
-        };
-
-        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-        for(int i = 0; i < data.length; i ++) {
-            bst.add(data[i]);
-        }
-
-        BinaryTrees.println(bst);
-
-//        bst.remove(1);
-//        bst.remove(2);
-//        bst.remove(12);
-
-//        bst.remove(11);
-
-//        bst.remove(9);
         bst.remove(7);
 
         BinaryTrees.println(bst);
     }
 
     public static void main(String[] args) {
-        test7();
+        test1();
     }
 }
