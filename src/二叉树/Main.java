3f181f045f1db2647f719244cfef69465313ff34
lizhiqiang <lizhiqiang@dyrs.com.cn>
2021-06-29 19:59:15 +0800

    avl树删除后平衡

--- src/二叉树/Main.java
+++ src/二叉树/Main.java
@@ -4,15 +4,19 @@ import 二叉树.printer.BinaryTrees;
 public class Main {
     static void test1(){
         Integer data[] = new Integer[] {
-                85, 19, 69, 3, 7, 99, 95, 2, 1, 70, 44, 58, 11, 21, 14, 93, 57, 4, 56
+//                85, 19, 69, 3, 7, 99, 95, 2, 1, 70, 44, 58, 11, 21, 14, 93, 57, 4, 56
+                85, 19, 69, 3, 7, 99, 95
         };
 
-        AVLTree<Integer> bst = new AVLTree<>();
+        AVLTree<Integer> avl = new AVLTree<>();
         for(int i = 0; i < data.length; i ++) {
-            bst.add(data[i]);
+            avl.add(data[i]);
 //            BinaryTrees.println(bst);
         }
-        BinaryTrees.println(bst);
+        avl.remove(99);
+        avl.remove(85);
+        avl.remove(95);
+        BinaryTrees.println(avl);
     }
 
     public static void main(String[] args) {
