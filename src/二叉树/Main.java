c0494edbd060fe84a05a4d6d6a648422056aad7d
lizhiqiang <lizhiqiang@dyrs.com.cn>
2021-07-07 19:17:27 +0800

    编辑打印

--- src/二叉树/Main.java
+++ src/二叉树/Main.java
@@ -5,12 +5,13 @@ public class Main {
     static void test1(){
         Integer data[] = new Integer[] {
 //                85, 19, 69, 3, 7, 99, 95, 2, 1, 70, 44, 58, 11, 21, 14, 93, 57, 4, 56
-                55, 87, 56, 74, 96, 22, 62, 20, 70, 68, 90, 50
+                85, 19, 69, 3, 7, 99, 95
         };
 
         RBTree<Integer> rb = new RBTree<>();
         for(int i = 0; i < data.length; i ++) {
             rb.add(data[i]);
+            BinaryTrees.println(rb);
         }
         BinaryTrees.println(rb);
     }
